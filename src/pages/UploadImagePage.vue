583cd827f0f90aadf1fb46c2619c4eb1ad5b9254
Tobias Olofsson <Tobias.Olofsson@sosalarm.se>
2022-01-09 14:08:35 +0100

    Remove starting templates. Added fab-button.

---
+++ src/pages/UploadImagePage.vue
@@ -0,0 +1,42 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Ta kort på vinlista</ion-title>
+                </ion-toolbar>    
+        </ion-header>
+        <ion-content>
+            <ion-fab vertical="center" horizontal="center" slot="fixed">
+                <ion-fab-button>
+                    <ion-icon :icon="cameraOutline" size="large"></ion-icon>
+                </ion-fab-button>
+            </ion-fab>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon } from "@ionic/vue";
+
+import { 
+  cameraOutline
+} from 'ionicons/icons';
+
+export default {
+components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFab, 
+    IonFabButton,
+    IonIcon 
+},
+setup() {
+    return {
+      cameraOutline
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,14 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import UploadImagePage from '../pages/UploadImagePage.vue';
 
 const routes = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/upload'
   },
   {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
+    path: '/upload',
+    component: UploadImagePage
   }
 ]
 
